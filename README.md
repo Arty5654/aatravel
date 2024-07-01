ffc1e088a6ba9ca565d17731873e7cacee29320e
Arty Avetissian <itsarty5654@gmail.com>
2024-06-30 20:07:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# aatravel
+To start virtual enviroment: pipenv shell
+To install dependencies: pipenv install
+To run server: python3 manage.py runserver
---
+++ aatravel-backend/api/views.py
@@ -0,0 +1,9 @@
+from .models import Account
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializer import AccountSerializer
+
+class AccountViewSet(viewsets.ModelViewSet):
+  queryset = Account.objects.all()
+  serializer_class = AccountSerializer
+  permissions_classes = [permissions.IsAuthenticated]
